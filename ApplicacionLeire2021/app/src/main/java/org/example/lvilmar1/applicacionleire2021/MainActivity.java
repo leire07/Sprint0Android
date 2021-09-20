db8fd25524836251ea9a2af5eb09d2592aacbe23
Leire <lvilmar1@epsg.upv.es>
2021-09-20 18:18:41 +0200

    Commit inicial

---
+++ ApplicacionLeire2021/app/src/main/java/org/example/lvilmar1/applicacionleire2021/MainActivity.java
@@ -0,0 +1,14 @@
+package org.example.lvilmar1.applicacionleire2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
